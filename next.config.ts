6bf2113347f65953ac034919de946513a49080a8
JosephChege4 <chegejrmungai@gmail.com>
2025-04-12 03:04:35 -0400

    initialized project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    mdxRs: true,
+  },
+  turbopack: {
+    // configure later
+  },
+}
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig)
